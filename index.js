d0669765f0673446b0e667835b197a2403bcf4e5
magiwanders <magi.wanders@gmail.com>
2022-10-29 14:08:39 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,75 @@
+import init from './pkg/runtime_experiments.js';
+import draw_plot from './plot.js';
+
+var rust
+window.test = test
+window.run_tests = run_tests
+
+init().then((wasm)=>{
+    console.log('Rust WASM Initialized.')
+    rust = wasm
+//    var P = 10, N = 40;
+//    test(P, N)
+})
+
+export function run_tests() {
+    var data = {
+        'js_time': [],
+        'go_time': [],
+        'rust_time': [],
+        'P': [],
+        'N': []
+    }
+
+    for (var P = 0; P <= 25; P+=10) {
+        for (var N = 0; N <= 50; N+=10) {
+            data.P.push(P)
+            data.N.push(N)
+            var times = test(P, N)
+            data.js_time.push(times.js)
+            data.go_time.push(times.go)
+            data.rust_time.push(times.rust)
+        }
+    }
+
+    draw_plot(data)
+}
+
+export function test(P, N) {
+    const start = performance.now();
+    js_test(P, N)
+    const end1 = performance.now();
+    go_test(P, N)
+    const end2 = performance.now();
+    rust.rust_test(P, N)
+    const end3 = performance.now();
+
+    var times = {
+        'js': ((end1-start)/1000),
+        'go': ((end2-end1)/1000),
+        'rust': ((end3-end2)/1000)
+    }
+
+    console.log("RESULTS FOR: ", P, N)
+    console.log("JS: " + times.js + ' s')
+    console.log("Go: " + times.go + ' s')
+    console.log("Rust: " + times.rust + ' s')
+
+    return times
+}
+
+function js_test(P, N) {
+    var a = 1
+    var b = 1
+    for (var p = 0; p < P; p++) {
+        a = Math.random()
+        for (var i = 0; i < N; i++) {
+            for (var j = 0; j < N; j++) {
+                for (var k = 0; k < N; k++) {
+                    b += b*a
+                }
+            }
+        }
+    }
+    return b
+}
---
+++ plot.js
@@ -0,0 +1,51 @@
+export default function draw_plot(data) {
+    var plot_data = [
+        {
+          name: 'Javascript',
+          showlegend: true,
+          opacity:0.2,
+          color:'green',
+          type: 'mesh3d',
+          x: data.P,
+          y: data.N,
+          z: data.js_time,
+        },
+          {
+          name: 'Go',
+          showlegend: true,
+          opacity:0.2,
+          color:'blue',
+          type: 'mesh3d',
+          x: data.P,
+          y: data.N,
+          z: data.go_time,
+        },
+           {
+           name: 'Rust',
+           showlegend: true,
+           opacity:0.2,
+           color:'red',
+           type: 'mesh3d',
+           x: data.P,
+           y: data.N,
+           z: data.rust_time,
+         }
+    ];
+
+    var layout = {
+    	scene: {
+    		xaxis:{title: 'P (number of NxN matrices)'},
+    		yaxis:{title: 'N (Size of square 2D Matrix)'},
+    		zaxis:{title: 'Mock Multiplication Time (s)'},
+    		},
+        width: 1000,
+        height: 800,
+        showlegend: true,
+        legend: {
+            x: 1,
+            xanchor: 'right',
+            y: 1
+          }
+    }
+    Plotly.newPlot('3d_plot', plot_data, layout);
+}
